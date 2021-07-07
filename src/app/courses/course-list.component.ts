5ab313940f79d8d6030fe563215021472ca84bad
Erica Noronha <erica.noronha@live.com>
2021-07-07 20:15:17 -0300

    0.4
    trata eventos data binding e transforma data com Pipes

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
+import { CourseService } from "./course.service";
 
 @Component({
     selector: 'app-course-list',
@@ -7,35 +8,25 @@ import { Course } from "./course";
 })
 export class CourseListComponent implements OnInit {
 
-    //courses: Course[] = []; 
-    // esse trecho acima deu erro, então usei a alternativa abaixo,
-    // me baseando neste link https://living-sun.com/pt/javascript/392655-property-39hellip39-has-no-initializer-and-is-not-definitely-assigned-in-the-constructor-javascript-angular-typescript.html
+    _courses: Course[];
+    _filterBy: string;
+    filteredCourses: Course[] = [];
 
-    courses!: Course[];
+    //injeção de dependência
+    constructor(private courseService: CourseService) { }
     
     ngOnInit(): void {
-        this.courses = [
-            {
-                id: 1,
-                name: 'Angular: Forms',
-                imageUrl: '/assets/images/forms.png',
-                price: 99.99,
-                code: 'XPS-8796',
-                duration: 120,
-                rating: 4.5,
-                releaseDate: 'December, 2, 2019'
-            },
-            {
-                id: 2,
-                name: 'Angular: HTTP',
-                imageUrl: '/assets/images/http.png',
-                price: 45.99,
-                code: 'LKL-1094',
-                duration: 80,
-                rating: 4,
-                releaseDate: 'December, 4, 2019'
-            }
-        ]
+        this._courses = this.courseService.retrieveAll();
+        this.filteredCourses = this._courses;
+    }
+
+    set filter(value: string) {
+        this._filterBy = value;
+        this.filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1);
+    }
+
+    get filter() {
+        return this._filterBy;
     }
 
 }
